df5b7e29263052fcdf3ff0635361fd8690918bf0
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-08-28 17:19:16 +0100

    tot of adding shit

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,8 @@ import 'package:firebase_blog_app/src/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+  // await FirebaseStorage(storageBucket: 'gs://blogapp-8f38d.appspot.com/');
   runApp(App());
 }
